59e20d3b6d37a4cc848cc35347c75dd1a8c8ff66
Elena Schweitzer <elenaschweitzer@MacBook-Air-von-Elena.fritz.box>
2025-04-27 19:49:40 +0200

    BMW is black

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Factory factory = new Factory();
 
-        Car car1 = new Car("red", "BMW", 5);
+        Car car1 = new Car("black", "BMW", 5);
         Car car2 = new Car("green", "Audi", 3);
         Car car3 = new Car("blue", "VW", 3);
 
